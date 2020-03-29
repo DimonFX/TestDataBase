81544fe3e034fcfb6a64024111cc636dd183239c
Шредер Дмитрий <DimonFX@yandex.ru>
2020-03-29 12:59:01 +0500

    ))

--- TestDataBaseProjiect/Table/Persons.sql
+++ TestDataBaseProjiect/Table/Persons.sql
@@ -4,8 +4,7 @@
     [Surname] NVARCHAR(50) NOT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Patronumic] NVARCHAR(50) NULL, 
-    [DateOfBirth] DATE NULL, 
-    [DateInput] DATE NULL DEFAULT getdate()
+    [DateOfBirth] DATE NULL
 )
 
 GO
@@ -62,12 +61,3 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'Persons',
     @level2type = N'COLUMN',
     @level2name = N'DateOfBirth'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Дата добавления записи',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'Persons',
-    @level2type = N'COLUMN',
-    @level2name = N'DateInput'
