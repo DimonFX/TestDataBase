c8967df058155db5aa6755c2a55e92ceb50f676a
Шредер Дмитрий <DimonFX@yandex.ru>
2020-03-29 11:46:10 +0500

    #TDP-4 Добавление  ХП к таблице Persons
    Создание процедур для работы с таблицей Persons

---
+++ TestDataBaseProjiect/sp_Persons_Select.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[sp_Persons_Select]
+	@Id int	--Идентификатор лица
+AS
+BEGIN
+	SELECT Persons.Id
+			,Persons.Surname
+			,Persons.Name
+			,Persons.Patronumic
+			,Persons.DateOfBirth
+		FROM dbo.Persons
+	where Persons.Id = @Id
+END
